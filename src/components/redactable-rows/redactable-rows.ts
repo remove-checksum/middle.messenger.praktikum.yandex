264f9028f211f2a95032bf18246e22f65454ec2a
German <bz5h7@yandex.ru>
2022-10-05 17:56:56 +0300

    feat: redactable-rows component

---
+++ src/components/redactable-rows/redactable-rows.ts
@@ -0,0 +1,51 @@
+import { Block } from "../../core"
+import "./redactable-rows.css"
+import { InputDefinition } from "../../models/inputDefinition"
+
+interface InputDataProps {
+  fields: Record<string, InputDefinition>
+  inactive: boolean
+}
+
+interface InputValidationProps {
+  isValid: boolean
+  hasLabel: boolean
+  label: string
+}
+
+type RedactableRowsProps = InputValidationProps & InputDataProps
+
+export class RedactableRows extends Block<RedactableRowsProps> {
+  static blockName = "RedactableRows"
+
+  constructor(props: RedactableRowsProps) {
+    super({
+      ...props,
+    })
+  }
+
+  render(): string {
+    return /* html */ `
+      <ul class="redactableRowsUserInfo">
+        {{#each fields as |field|}}
+          {{#if field.input_type}}
+            <li class="redactableRowsUserInfo__row">
+              <span class="redactableRowsUserInfo__text">{{field.label}}</span>
+              {{{ ControlledInput hasLabel=hasLabel
+                disabled=@root.inactive
+                onFocus=@root.onFocus
+                onBlur=@root.onBlur
+                label=label
+                type=field.input_type
+                placeholder=placeholder
+                id=field.label
+                name=field.name
+                extraClass="redactableRowsUserInfo__input"
+                ref="inputRef"
+              }}}
+          {{/if}}
+        {{/each}}
+      </ul>
+    `
+  }
+}
