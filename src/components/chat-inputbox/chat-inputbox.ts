8fd32f4dff2d2a79f9f92886d14f0abc9e7dd978
German <bz5h7@yandex.ru>
2022-10-05 17:55:33 +0300

    feat: chat-inputbox component

---
+++ src/components/chat-inputbox/chat-inputbox.ts
@@ -0,0 +1,31 @@
+import { Block } from "../../core"
+import "./chat-inputbox.css"
+
+interface ChatInputboxProps {
+  inputType: string
+  inputName: string
+}
+
+export class ChatInputbox extends Block<ChatInputboxProps> {
+  static blockName = "ChatInputbox"
+
+  render() {
+    return /* html */ `
+      <form action="#" class="chatInputboxForm">
+        <button class="chatInputboxForm__button">
+          <i class="ph-paperclip chatInputboxForm__icon"></i>
+        </button>
+        {{{ ControlledInput
+          name=inputName
+          hasLabel=false
+          extraClass="chatInputboxForm__input"
+          type=chatInputType
+          placeholder="Введите сообщение"
+        }}}
+        <button class="chatInputboxForm__button">
+          <i class="ph-paper-plane-right chatInputboxForm__icon"></i>
+        </button>
+      </form>
+    `
+  }
+}
