17ba7dc3c3ca496d5a0e085cc1a765a0c9b092c8
German <bz5h7@yandex.ru>
2022-10-05 17:59:44 +0300

    feat: not-found page

---
+++ src/pages/404/404.ts
@@ -0,0 +1,18 @@
+import { Block } from "../../core"
+import "./404.css"
+
+export class NotFoundPage extends Block<EmptyObject> {
+  static blockName = "NotFoundPage"
+
+  render() {
+    return /* html */ `
+      {{#PageLayout}}
+        <div class="errorPage">
+          <h1 class="errorPage__code">400</h1>
+          <h2 class="errorPage__subheading">Не туда попали</h2>
+          <a href="./chat">Назад к чатам</a>
+        </div>
+      {{/PageLayout}}
+    `
+  }
+}
