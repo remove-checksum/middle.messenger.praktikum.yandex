0349175fe5c786274bad03f17d7da73a3d3033bf
German <bz5h7@yandex.ru>
2022-10-15 00:34:35 +0300

    style: uppercase matchers

--- src/services/validator/matchers.ts
+++ src/services/validator/matchers.ts
@@ -1,13 +1,10 @@
-const nameMatcher = /([a-zа-яё]*)[a-zа-яё-]*([a-zа-яё]*)*$/i // english / russian / maybe hyphen in the middle
-const loginMatcher = /^([a-zа-яё]*)[a-zа-яё_-]*([a-zа-яё]*)*$/i // english / russian / maybe hyphen or underscore in the middle
-const emailMatcher = /^[a-z0-9]+@[a-z0-9]+\.([a-z]){2,}$/i // english / '@' in the middle / domain name > 2 after dot
-const passwordMatcher = /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,40}$/ // english / numbers / at least one capital letter and number
-const phoneMatcher = /^\+?([0-9])*$/i // 10-15 numbers, may have '+' in the beginning
-
-export {
-  nameMatcher,
-  loginMatcher,
-  emailMatcher,
-  passwordMatcher,
-  phoneMatcher,
+// prettier-ignore
+const MATCHERS = {
+  NAME: /([a-zа-яё]*)[a-zа-яё-]*([a-zа-яё]*)*$/i,    // english / russian / maybe hyphen in the middle
+  LOGIN: /^([a-zа-яё]*)[a-zа-яё_-]*([a-zа-яё]*)*$/i, // english / russian / maybe hyphen or underscore in the middle
+  EMAIL: /^[a-z0-9]+@[a-z0-9]+\.([a-z]){2,}$/i,      // english / '@' in the middle / domain name > 2 after dot
+  PASSWORD: /^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,40}$/, // english / numbers / at least one capital letter and number
+  PHONE: /^\+?([0-9])*$/i,                           // 10-15 numbers, may have '+' in the beginning
 }
+
+export { MATCHERS }
--- src/services/validator/validators.ts
+++ src/services/validator/validators.ts
@@ -1,42 +1,37 @@
 import errorMessages from "./errorMessages.json"
-import {
-  emailMatcher,
-  loginMatcher,
-  nameMatcher,
-  passwordMatcher,
-  phoneMatcher,
-} from "./matchers"
 import { from, upto, notEmpty } from "./helpers"
+import { MATCHERS } from "./matchers"
 
 export interface Validator {
   (value: string): string | null
 }
 
 const passwordValidator: Validator = (value) => {
-  const valid = from(8)(value) && upto(40)(value) && passwordMatcher.test(value)
+  const valid =
+    from(8)(value) && upto(40)(value) && MATCHERS.PASSWORD.test(value)
 
   return valid ? "" : errorMessages.password.characters
 }
 
 const nameValidator: Validator = (value) => {
-  const valid = notEmpty(value) && upto(20)(value) && nameMatcher.test(value)
+  const valid = notEmpty(value) && upto(20)(value) && MATCHERS.NAME.test(value)
 
   return valid ? "" : errorMessages.name.characters
 }
 
 const loginValidator: Validator = (value) => {
-  const valid = from(3)(value) && upto(20)(value) && loginMatcher.test(value)
+  const valid = from(3)(value) && upto(20)(value) && MATCHERS.LOGIN.test(value)
 
   return valid ? "" : errorMessages.login.characters
 }
 
 const emailValidator: Validator = (value) => {
-  const valid = upto(40)(value) && emailMatcher.test(value)
+  const valid = upto(40)(value) && MATCHERS.EMAIL.test(value)
   return valid ? "" : errorMessages.email.characters
 }
 
 const phoneValidator: Validator = (value) => {
-  const valid = from(10)(value) && upto(15)(value) && phoneMatcher.test(value)
+  const valid = from(10)(value) && upto(15)(value) && MATCHERS.PHONE.test(value)
   return valid ? "" : errorMessages.phone.characters
 }
 
