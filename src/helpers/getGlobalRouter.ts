9d5efc21b68a3b40e6537afa1ac150ae89e05499
German <bz5h7@yandex.ru>
2022-11-15 00:29:05 +0300

    refactor: withStore hoc optimisations

--- src/helpers/getGlobalRouter.ts
+++ src/helpers/getGlobalRouter.ts
@@ -6,3 +6,12 @@ export const getGlobalRouter = () => {
 
   return router
 }
+
+export const getGlobalStore = () => {
+  const { store } = window.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+  if (!store) {
+    throw new Error("Router not in global")
+  }
+
+  return store
+}
--- src/hoc/withStore.ts
+++ src/hoc/withStore.ts
@@ -1,43 +1,43 @@
 import { AppState, AppStore } from "../store/store"
 import { BlockProps } from "../core/Block"
 import { StoreEvents, Block } from "../core"
+import { getGlobalStore } from "../helpers"
 
 export type StoreContext = { store: AppStore }
-type StateMapper = (state: AppState) => Partial<AppState>
+type StateMapper = (state: AppState) => UnknownObject
 
-export function withStore<S extends StoreContext>(
-  WrappedBlock: typeof Block,
-  mapStateToProps: StateMapper
-) {
-  type WrappedProps = typeof WrappedBlock extends typeof Block<
-    infer P & AnyObject & BlockProps
-  >
-    ? P & StoreContext
-    : S
+export function withStore(mapStateToProps: StateMapper) {
+  return function withPreviousState(WrappedBlock: typeof Block) {
+    const store = getGlobalStore()
 
-  // @ts-expect-error WrappedBlock has render()
-  return class BlockWithStore<P extends WrappedProps> extends WrappedBlock<P> {
-    static blockName = WrappedBlock.blockName
+    const previousState = mapStateToProps(store.getState())
 
-    constructor(props: P) {
-      const { store } =
-        window.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+    // @ts-expect-error WrappedBlock has render()
+    return class BlockWithStore<
+      P extends BlockProps & StoreContext
+    > extends WrappedBlock<P> {
+      static blockName = WrappedBlock.blockName
 
-      super({ ...props, store, ...mapStateToProps(store.getState()) })
-    }
+      constructor(props: P) {
+        super({
+          ...props,
+          ...previousState,
+        })
+      }
 
-    private onStoreChange = () => {
-      this.setProps(mapStateToProps(this.props.store.getState()) as P)
-    }
+      private onStoreChange = () => {
+        this.setProps(mapStateToProps(this.props.store.getState()) as P)
+      }
 
-    componentDidMount(props: P): void {
-      super.componentDidMount(props)
-      this.props.store.on(StoreEvents.Updated, this.onStoreChange)
-    }
+      componentDidMount(props: P): void {
+        super.componentDidMount(props)
+        this.props.store.on(StoreEvents.Updated, this.onStoreChange)
+      }
 
-    componentWillUnmount(props: P): void {
-      super.componentWillUnmount(props)
-      this.props.store.off(StoreEvents.Updated, this.onStoreChange)
+      componentWillUnmount(props: P): void {
+        super.componentWillUnmount(props)
+        this.props.store.off(StoreEvents.Updated, this.onStoreChange)
+      }
     }
   }
 }
