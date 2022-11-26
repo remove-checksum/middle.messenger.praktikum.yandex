8b43aa8aefed429819f8c5da5ee91cedd264b3bc
German <bz5h7@yandex.ru>
2022-11-26 22:58:56 +0300

    fix: server resolves GET to index

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express')
+const path = require('path')
 
 const PORT = 3000;
 
@@ -7,10 +8,13 @@ const app = express()
 
 app.use(express.static('./dist'))
 
+
+const spaPath = path.resolve(__dirname, '../dist/index.html')
+
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 })
 
-app.get('*', (req, res) => {
-  res.sendFile('dist/index.html', { root: __dirname })
+app.get('/*', (req, res) => {
+  res.sendFile(spaPath)
 })
